5990ed03add71a4547cdccc9e2d24c37485df5e6
Inkom007 <inkom0703@gmail.com>
2024-04-25 22:04:36 +0100

    task-3

---
+++ js/task-3.js
@@ -0,0 +1,11 @@
+const input = document.querySelector("#name-input");
+const output = document.querySelector("#name-output");
+
+
+input.addEventListener("input", (event) => {
+    output.textContent = event.currentTarget.value.trim();
+   
+});
+
+console.log(input);
+console.log(output);
