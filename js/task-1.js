662746f1d644cc55db1d6735adfa90570a8d3a4d
Inkom007 <inkom0703@gmail.com>
2024-04-27 21:31:35 +0100

    correction task-3

--- js/task-1.js
+++ js/task-1.js
@@ -2,12 +2,12 @@ const item = document.querySelectorAll("li.item");
 console.log(`Number of categories: ${item.length}`);
 
 
-
  
 item.forEach(category => {
     const categoryName = category.querySelector("h2").textContent;
     const categoryAmount = category.querySelectorAll("li").length;
     console.log(`Category: ${categoryName}`);
     console.log(`Elements: ${categoryAmount}`);
-});
+    
 
+});
--- js/task-3.js
+++ js/task-3.js
@@ -2,7 +2,12 @@ const input = document.querySelector("#name-input");
 const output = document.querySelector("#name-output");
 
 
-input.addEventListener("input", (event) => {
-    output.textContent = event.currentTarget.value.trim();
-   
+input.addEventListener("input", event => {
+    if(output.textContent = event.currentTarget.value.trim()) {
+        return; 
+    }
+    else {
+        return output.textContent = `Anonymous`;
+    }
+    
 });
